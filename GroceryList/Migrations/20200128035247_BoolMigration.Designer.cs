9ad62ca5cc91bc26507e2e3dedad8c3131369f05
acnaumann <alysson12985@gmail.com>
2020-01-27 21:55:12 -0600

    new migration BoolMigration

--- GroceryList/Migrations/20200128035247_BoolMigration.Designer.cs
+++ GroceryList/Migrations/20200128035247_BoolMigration.Designer.cs
@@ -2,14 +2,16 @@
 using GroceryList.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GroceryList.Migrations
 {
     [DbContext(typeof(ListDbContext))]
-    partial class ListDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200128035247_BoolMigration")]
+    partial class BoolMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
